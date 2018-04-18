e2445d620eeb15c30598419fdd00b100dee4fbe6
Chloe Chen <qchen7@tulane.edu>
2018-04-18 00:32:00 -0500

    Added one-vs-all classification for each toxic class

--- mlproject/classification.py
+++ mlproject/classification.py
@@ -8,9 +8,9 @@ from nltk.corpus import stopwords
 from nltk.tokenize.casual import TweetTokenizer
 import gensim
 from sklearn import svm
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
-
+#from sklearn import MLPClassifier
 stop = set(stopwords.words('english'))
 html_parser = HTMLParser.HTMLParser()
 tknzr = TweetTokenizer()
@@ -23,6 +23,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 """DATA READER"""
+
 # load the training data
 data = pd.read_csv("train.csv")
 #print data['id'][6]
@@ -30,10 +31,8 @@ data = pd.read_csv("train.csv")
 
 # create one vector for the six categories of toxicity
 data['toxicity_vec'] = [[a, b, c, d, e, f] for a, b, c, d, e, f in zip(data['toxic'], data['severe_toxic'], data['obscene'], data['threat'], data['insult'], data['identity_hate'])]
-#print data['toxicity_vec'][6]
 
-
-
+logger.info("Data read.")
 def document_vector(word2vec_model, doc):
     # remove out-of-vocabulary words
     # calculate the mean vector for each tweet
@@ -43,7 +42,6 @@ def document_vector(word2vec_model, doc):
     else:
         #print np.zeros(1)
         return np.zeros(300)
-
 
 # create clean text data
 t = data['comment_text']
@@ -89,10 +87,17 @@ for s in range(len(data['tokens'])):
 with open('mean_vecs.txt','w') as f:
     f.writelines('\t'.join(str(j) for j in i) +'\n' for i in mean_vecs)
 
-# read mean vecs
+    
+# read mean vecs, if previous lines were run once, can start from here
 with open('mean_vecs.txt') as t:
     mean_vecs = [map(float, line.split()) for line in t]
 
+toxic = [i[0] for i in data['toxicity_vec']]
+sev_tox = [i[1] for i in data['toxicity_vec']]
+obscene = [i[2] for i in data['toxicity_vec']]
+threat = [i[3] for i in data['toxicity_vec']]
+insult = [i[4] for i in data['toxicity_vec']]
+id_hate = [i[5] for i in data['toxicity_vec']]
 # PREDICTION
 #
 #
@@ -101,15 +106,44 @@ logger.info("Loading classifier.")
 clf = svm.LinearSVC() # for one-vs-all/one-of
 #clf = MLPClassifier(solver='lbgfs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
 
-X_train, X_test, y_train, y_test = train_test_split(mean_vecs, data['toxicity_vec'], test_size=0.1, random_state=42)
-
+X_train, X_test, y_train, y_test = train_test_split(mean_vecs, toxic, test_size=0.1, random_state=42)
 
 #fit our classifier
 clf.fit(X_train,y_train)
 
 #predict on our test samples
 pred = clf.predict(X_test)
+#print clf.predict_proba(X_test)
 
 print classification_report(y_test, pred)
 
 
+sev_tox_clf = svm.LinearSVC()
+X_train1, X_test1, y_train1, y_test1 = train_test_split(mean_vecs, sev_tox, test_size=0.1, random_state=42)
+sev_tox_clf.fit(X_train1,y_train1)
+pred1 = sev_tox_clf.predict(X_test1)
+print classification_report(y_test1, pred1)
+
+obs_clf = svm.LinearSVC()
+X_train2, X_test2, y_train2, y_test2 = train_test_split(mean_vecs, obscene, test_size=0.1, random_state=42)
+obs_clf.fit(X_train2,y_train2)
+pred2 = obs_clf.predict(X_test2)
+print classification_report(y_test2, pred2)
+
+threat_clf = svm.LinearSVC()
+X_train3, X_test3, y_train3, y_test3 = train_test_split(mean_vecs, threat, test_size=0.1, random_state=42)
+threat_clf.fit(X_train3,y_train3)
+pred3 = threat_clf.predict(X_test3)
+print classification_report(y_test3, pred3)
+
+insult_clf = svm.LinearSVC()
+X_train4, X_test4, y_train4, y_test4 = train_test_split(mean_vecs, insult, test_size=0.1, random_state=42)
+insult_clf.fit(X_train4,y_train4)
+pred4 = insult_clf.predict(X_test4)
+print classification_report(y_test4, pred4)
+
+id_hate_clf = svm.LinearSVC()
+X_train5, X_test5, y_train5, y_test5 = train_test_split(mean_vecs, id_hate, test_size=0.1, random_state=42)
+id_hate_clf.fit(X_train5,y_train5)
+pred5 = id_hate_clf.predict(X_test5)
+print classification_report(y_test5, pred5)
